7f8b42adc7699a51c56d002c87be4dc4a23c2181
Hebah Turki <hebahturki@outlook.com>
2023-05-26 23:28:10 +0300

    Frist commit!

--- src/ustudy/Schedule.java
+++ src/ustudy/Schedule.java
@@ -9,9 +9,68 @@ import java.util.Date;
  */
 public class Schedule 
 {
-    LearningMaterial ML;
-    int ScheduleID;
-    ArrayList<Session> sessionList = new ArrayList<>();
-    Date date;
+    int LMID;
+    String LMName;
+    String date;
+    ArrayList<Schedule> schedule = new ArrayList();
+
+    public Schedule(int LMID, String LMName, String date) 
+    {
+        this.LMID = LMID;
+        this.LMName = LMName;
+        this.date = date;
+    }
+
+    public Schedule() {
+    }
     
+    public void SchedulePrint()
+    {
+        System.out.println("-------------------------------------------------");
+        System.out.println("                Study Schedule                   ");
+        System.out.println("-------------------------------------------------");
+        for (int i = 0; i < schedule.size(); i++) {
+
+            System.out.println("ID: " + schedule.get(i).LMID
+                    + "  Subject Name: " + schedule.get(i).getLMName()
+                    + "  Start Date: "+schedule.get(i).getDate());
+        }
+        System.out.println("-------------------------------------------------\n");
+    }
+
+    public int getLMID() {
+        return LMID;
+    }
+
+    public void setLMID(int LMID) {
+        this.LMID = LMID;
+    }
+
+    public String getLMName() {
+        return LMName;
+    }
+
+    public void setLMName(String LMName) {
+        this.LMName = LMName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public ArrayList<Schedule> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(ArrayList<Schedule> schedule) {
+        this.schedule = schedule;
+    }
+    
+    public void addSchedule(Schedule lm) {
+        schedule.add(lm);
+    }
 }
--- src/ustudy/UStudy.java
+++ src/ustudy/UStudy.java
@@ -1,6 +1,8 @@
 package ustudy;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -23,21 +25,21 @@ public class UStudy {
         System.out.println("5: Add material to a subject ");
         System.out.println("6: View Learning Material");
         System.out.println("7: Delete material of a subject ");
-        System.out.println("8: Update progress for a learning material: ");
-        System.out.println("9: Quit");
+        System.out.println("8: Update progress for a learning material ");
+        System.out.println("9: Start Schedule ");
+        System.out.println("10: Quit");
         System.out.println(" -------------------------------------------------------------------");
         System.out.print("\nChoose from the menu: ");
     }
 
-
-
     //---------------------------------------------------------------------------
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+
         Scanner input = new Scanner(System.in);
         int num; //number by user
         int id = 0; //subject ID
         Subject instance = new Subject();
+        Schedule schInstance = new Schedule();
         LearningMaterial lmInstance = new LearningMaterial();
 
         do {
@@ -61,10 +63,10 @@ public class UStudy {
                     for (int i = 0; i < instance.subjects.size(); i++) {
                         int pagesDone = lmInstance.pagesDoneforSubject(idForSearch);
                         int totalPages = lmInstance.totalPagesforSubject(idForSearch);
-                        instance.calculateProgress(pagesDone,totalPages,idForSearch);
+                        instance.calculateProgress(pagesDone, totalPages, idForSearch);
                         idForSearch++;
                     }
-                    
+
                     instance.viewSubject();
                     break;
 
@@ -146,33 +148,48 @@ public class UStudy {
                     }
                     break;
                 }
-                
-                case 8:{
+
+                case 8: {
                     System.out.print("Enter Subject ID: ");
                     num = input.nextInt();
-                    if (instance.existsSubject(num)){
+                    if (instance.existsSubject(num)) {
                         System.out.print("Enter the Learning Material ID to Update Pages Done: ");
                         num = input.nextInt();
-                        if(lmInstance.existMaterial(num)){
+                        if (lmInstance.existMaterial(num)) {
                             LearningMaterial lm = lmInstance.getLMbyId(num);
-                            System.out.print("Enter Number Pages Done in "+ lm.getName()+": " );
+                            System.out.print("Enter Number Pages Done in " + lm.getName() + ": ");
                             num = input.nextInt();
                             lm.setPages_done(num);
                             System.out.println("\nPages Done Updated Sucessfuly");
-                           
-                        }
-                        else
+
+                        } else {
                             System.out.println("Learning Material not found");
-                    }
-                    else
+                        }
+                    } else {
                         System.out.println("Subject not found");
+                    }
                     break;
                 }
-                case 9:
+                case 9: {
+                    instance.viewSubject();
+                    System.out.print("\nSelect Subject ID: ");
+                    num = input.nextInt();
+                    Subject subject = instance.getSubjectById(num);
+                    System.out.print("Enter Start Date(DD/MM/YYYY): ");
+                    String date = input.next();
+                    
+                    Schedule MLSchedule = new Schedule(subject.id, subject.getName(), date);
+                    schInstance.addSchedule(MLSchedule);
+                    
+                    schInstance.SchedulePrint();
+                    break;
+                }
+
+                case 10:
                     System.out.println(" > Exiting...");
                     System.out.println(" > Goodbye");
                     break;
-                    
+
                 default:
                     System.out.println();
                     System.out.println(" > Invalid choice entered! Please try again.");
